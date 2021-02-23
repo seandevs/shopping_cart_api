002f991645c47d25f283a729e6c4a6ca43b56c4d
System Administrator <sysadmin@hpc320.tcnj.edu>
2021-02-23 11:38:39 -0500

    update output

--- server.rb
+++ server.rb
@@ -9,21 +9,21 @@ db = DB.new
 
 # $ curl --location --request GET 'localhost:4567/'
 get '/' do
-  'Howdy. This is our shopping cart API.'
+  "Howdy. This is our shopping cart API.\n"
 end
 
 # $ curl --location --request GET 'localhost:4567/cart/1'
 get '/cart/:id' do
-  id = params['id']
-  cart = db.data[id.to_i]
-  "Your cart has #{cart.items}"
+  id = params['id'].to_i
+  cart = db.data[id]
+  "Cart with id #{id} has #{cart.items}.\n"
 end
 
 # $ curl --location --request POST 'localhost:4567/cart'
 post '/cart' do
   cart = Cart.new
   db.data << cart
-  "new cart id: #{db.data.length}"
+  "New cart with id #{db.data.length} has #{cart.items}.\n"
 end
 
 # $ curl --location --request PATCH 'localhost:4567/cart/1?soda=1'
@@ -40,7 +40,7 @@ patch '/cart/:id' do
   cart.items[:milk] += milk
   cart.items[:hotdogs] += hotdogs
   cart.items[:mustard] += mustard
-  "Your cart is #{cart.items}"
+  "Cart with id #{id} has #{cart.items}.\n"
 end
 
 # $ curl --location --request DELETE 'localhost:4567/cart/1'
@@ -48,5 +48,5 @@ delete '/cart/:id' do
   id = params['id'].to_i
 
   db.data.delete_at(id)
-  "cart wth id: #{id} is deleted. The DB now has #{db.data.length} records."
+  "Cart wth id #{id} is deleted. The DB now has #{db.data.length} records.\n"
 end
